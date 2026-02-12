055a1271b12c2c9b18c814dca055df7c7936ef6a
ultra-rage <amod@browserstack.com>
2026-02-12 17:23:51 +0530

    Fix Express routes for Vercel

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const tickets = require("./api/tickets.js");
-require("dotenv").config();
+const path = require("path");
 
 const app = express();
 
-// Serve static files (index.html, etc.)
-app.use(express.static(__dirname));
+// Serve static files from root
+app.use(express.static(path.join(__dirname)));
 
 // API route
 app.get("/api/tickets", tickets);
 
-// Export the app for Vercel
+// Catch-all route to serve index.html for SPA
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 module.exports = app;
